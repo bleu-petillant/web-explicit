77da8bbcec17a770b2c3e7090ad3580719d12ac2
Greniert <39262221+Greniert@users.noreply.github.com>
2020-12-11 18:49:55 +0100

    Update usage.blade.php

--- resources/views/usage.blade.php
+++ resources/views/usage.blade.php
@@ -1,4 +1,28 @@
 @extends('layouts.app')
 @section('usage')
-<h1>usage page</h1>
+
+
+<section class="">
+        <h1>Cas d'usage</h1>
+</section>
+
+<section class="cas-1">
+    <img src="" alt="">
+    <h2>RECHERCHE ET FORMATION À LA MÉDITATION</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro mollitia iure veniam dolores voluptates et amet ab error fuga incidunt, expedita suscipit temporibus maxime, asperiores ea. Laborum eos neque molestiae provident deserunt, harum fuga ducimus molestias aspernatur qui nulla saepe aliquid! Reprehenderit natus qui eaque reiciendis autem repudiandae at ex.</p>
+</section>
+
+<section class="cas-2">
+    <h2>RECHERCHE ET FORMATION EN STAPS</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro mollitia iure veniam dolores voluptates et amet ab error fuga incidunt, expedita suscipit temporibus maxime, asperiores ea. Laborum eos neque molestiae provident deserunt, harum fuga ducimus molestias aspernatur qui nulla saepe aliquid! Reprehenderit natus qui eaque reiciendis autem repudiandae at ex.</p>
+    <img src="" alt="">
+</section>
+
+
+<section class="cas-3">
+    <img src="" alt="">
+    <h2>FORMATION DES POMPIERS</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro mollitia iure veniam dolores voluptates et amet ab error fuga incidunt, expedita suscipit temporibus maxime, asperiores ea. Laborum eos neque molestiae provident deserunt, harum fuga ducimus molestias aspernatur qui nulla saepe aliquid! Reprehenderit natus qui eaque reiciendis autem repudiandae at ex.</p>
+</section>
+
 @endsection

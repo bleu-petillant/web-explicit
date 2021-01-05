6eee2bee831e7d1ce2d3ce53914bf19dd224ac9d
Greniert <39262221+Greniert@users.noreply.github.com>
2021-01-05 13:46:15 +0100

    header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
         @auth
         @livewire('navigation-dropdown')
 
-        <nav class="">
+<nav class="">
     <div class="h-16 px-2 sm:px-6 lg:px-8">
         <div class="relative flex items-center justify-between h-16">
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -105,8 +105,101 @@
                 <div>
                     <button class=" flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true">
                         <span class="sr-only">Open user menu</span>
-                        <i class="main-color-text text-xl fas fa-user"></i>
+        
                     </button>
+                        <div class="hidden sm:flex sm:items-center sm:ml-6">
+                            <x-jet-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out py-2">
+                                            <div>{{ Auth::user()->name }}</div>
+                                            <div class="ml-1">
+                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+                                        </button>
+
+                                </x-slot>
+
+                                <x-slot name="content">
+                                    <!-- Account Management -->
+                                    @if (auth()->user()->role_id == 1)
+                                    <x-jet-dropdown-link href="{{ route('admin.dashboard') }}" :active="request()->routeIs('admin.dashboard')">
+                                        {{ __('Dashboard') }}
+                                    </x-dropdown-link>
+                                            @elseif(auth()->user()->role_id == 2)
+                                    <x-jet-dropdown-link href="{{ route('teacher.dashboard') }}" :active="request()->routeIs('teacher.dashboard')">
+                                        {{ __('Dashboard') }}
+                                    </x-jet-dropdown-link>
+                                    @endif
+                                    <div class="block px-4 py-2 text-xs text-gray-400">
+                                        {{ __('Manage Account') }}
+                                    </div>
+                                    @if(auth()->user()->role_id == 1)
+                                    <x-jet-dropdown-link href="{{ route('admin.dashboard') }}">
+                                        {{ __('Profile') }}
+                                    </x-jet-dropdown-link>
+                                    @elseif(auth()->user()->role_id == 2)
+                                    <x-jet-dropdown-link href="{{ route('teacher.dashboard') }}">
+                                        {{ __('Profile') }}
+                                    </x-jet-dropdown-link>
+                                    @else
+                                    <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                        {{ __('Profile') }}
+                                    </x-jet-dropdown-link>
+                                    @endif
+                                    @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                                        <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
+                                            {{ __('API Tokens') }}
+                                        </x-jet-dropdown-link>
+                                    @endif
+
+                                    <div class="border-t border-gray-100"></div>
+
+                                    <!-- Team Management -->
+                                    @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                                        <div class="block px-4 py-2 text-xs text-gray-400">
+                                            {{ __('Manage Team') }}
+                                        </div>
+
+                                        <!-- Team Settings -->
+                                        <x-jet-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
+                                            {{ __('Team Settings') }}
+                                        </x-jet-dropdown-link>
+
+                                        @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
+                                            <x-jet-dropdown-link href="{{ route('teams.create') }}">
+                                                {{ __('Create New Team') }}
+                                            </x-jet-dropdown-link>
+                                        @endcan
+
+                                        <div class="border-t border-gray-100"></div>
+
+                                        <!-- Team Switcher -->
+                                        <div class="block px-4 py-2 text-xs text-gray-400">
+                                            {{ __('Switch Teams') }}
+                                        </div>
+
+                                        @foreach (Auth::user()->allTeams() as $team)
+                                            <x-jet-switchable-team :team="$team" />
+                                        @endforeach
+
+                                        <div class="border-t border-gray-100"></div>
+                                    @endif
+
+                                    <!-- Authentication -->
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+
+                                        <x-jet-dropdown-link href="{{ route('logout') }}"
+                                                            onclick="event.preventDefault();
+                                                                        this.closest('form').submit();">
+                                            {{ __('Logout') }}
+                                        </x-jet-dropdown-link>
+                                    </form>
+                                </x-slot>
+                            </x-jet-dropdown>
+                        </div>
                 </div>
 
             </div>
@@ -183,7 +276,7 @@
                 <div>
                     <button class=" flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true">
                         <span class="sr-only">Open user menu</span>
-                        <i class="main-color-text text-xl fas fa-user"></i>
+                        <a href="{{route('login')}}" class="text-gray px-3 py-2 rounded-md text-lg font-medium"><i class="main-color-text text-xl fas fa-user"></i></a>
                     </button>
                 </div>
 
